2afa71712fc43713b5252768490a663be79ece2a
Sheelagh Ikechi <sheelagh@gmail.com>
2025-08-01 22:13:41 +0100

    Add root route

--- index.js
+++ index.js
@@ -8,5 +8,9 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/files', fileRoutes);
 
+app.get('/', (req, res) => {
+  res.send('Welcome to the File Upload API!');
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
